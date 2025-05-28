2e57e850393db4cb65c6c07e465ec79c2a318058
RizalFebrian16 <itsrizalfebrian@gmail.com>
2025-05-28 11:20:44 +0700

    menambahkan bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,17 @@
-rz
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>HALLO RIZAL</p>
+    <td>
+        <tr>
+            <a href="/tampil">Dashboard</a>
+        </tr>
+    </td>
+</body>
+</html>
